f52881200ea04218617782961ca40c1cca82e673
Eric Snover <ersnover@gmail.com>
2019-06-26 10:26:02 -0500

    Take 4

---
+++ prac1.py
@@ -0,0 +1,7 @@
+names = ["Frodo", "Sam", "Pippin", "Merry", "Aragorn", "Boromir", "Legolas", "Gimli", "Gandalf the muhfuckin Grey"]
+for name in names:
+    if not name == "Boromir":
+        print(f"{name} did a great job on this mission.")
+    else:
+        print(f"{name} was involved.")
+    
